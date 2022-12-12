f4ec1cc1f37235fcc053bdfd04492cb6c7a01247
farmaciatorres <120394785+farmaciatorres@users.noreply.github.com>
2022-12-12 09:36:22 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Banner from './components/Banner/Banner';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Banner/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,37 @@
+import { MegaphoneIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+export default function Banner() {
+  return (
+    <div className="bg-blue-600">
+      <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap items-center justify-between">
+          <div className="flex w-0 flex-1 items-center">
+            <span className="flex rounded-lg bg-blue-800 p-2">
+              <MegaphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            </span>
+            <p className="ml-3 truncate font-medium text-white">
+              <span className="hidden md:inline">En FARMACIA TORRES tenemos todo lo que nesecitas para tu bienestar.</span>
+            </p>
+          </div>
+          <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="#"
+              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-blue-800 shadow-sm hover:bg-indigo-50"
+            >
+              Ver más
+            </a>
+          </div>
+          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+            >
+              <span className="sr-only">Dismiss</span>
+              <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
